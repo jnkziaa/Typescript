b50c08602315cdb72276e15b2f132cd84c8cb5ed
John Albesa <107880782+jnkziaa@users.noreply.github.com>
2022-11-07 14:58:19 -0800

    typescripts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ function reverse() {
 
 reverse();
 
-function calculateValue(number) {
+function calculateValue(number: number) {
     if(number < 1) {
         return 0;
     }
@@ -35,62 +35,19 @@ for (var n = 0; n < 10; ++n) {
 
 calculateValue(5);
 
+export {}
+let message = "Welcome Back";
+console.log(message);
 
-const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
-const reviewTotalDisplay = document.querySelector('#reviews')
-let isOpen: boolean
+let x = 10;
+const y = 20;
+let sum;
+const title = "Codevolution";
 
-const reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-]
+let isBeginner : boolean = true;
+let total : number = 0;
+let name : string = "John"
 
+let sentence : string = `my name is ${name}`;
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
-const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[]
-} = {
-    firstName: 'Bobby',
-    lastName: 'Brown',
-    isReturning: true,
-    age: 35,
-    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
-
-
-function populateUser(isReturning : boolean, userName: string ) {
-    if (isReturning == true){
-        returningUserDisplay.innerHTML = 'back'
-    }
-    userNameDisplay.innerHTML = userName
-}
-
-populateUser(you.isReturning, you.firstName)
-
+console.log(sentence);
